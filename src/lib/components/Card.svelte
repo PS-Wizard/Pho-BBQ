e00bcb5981c812e2c6508239e495fb92bf4a21e4
Apsan1 <apsanrana61@gmail.com>
2025-09-07 19:07:29 +0545

    fixed card height and removed animations

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import TextMask from "./TextMask.svelte";
 
-    let { image, title, description, href = "" } = $props();
+  let { image, title, description, href = "" } = $props();
 </script>
 
-<a {href} class="block w-full overflow-hidden space-y-4">
-    <img src={image} alt={title} class="w-full h-[32em] object-cover" />
-    <div class="gap-2 flex flex-col">
-        <TextMask styles="text-2xl clash tracking-wide">
-            {title}
-        </TextMask>
-        <TextMask styles="text-neutral-500 tracking-wide w-full sm:inline">
-            {description}
-        </TextMask>
-    </div>
+<a {href} class="block w-full overflow-hidden space-y-2">
+  <img src={image} alt={title} class="w-full h-[24em] object-cover" />
+  <div class="gap-2 flex flex-col">
+    <p class="text-2xl tracking-wide">
+      {title}
+    </p>
+    <p class="text-[14px] text-neutral-500 tracking-wide w-full sm:inline">
+      {description}
+    </p>
+  </div>
 </a>
